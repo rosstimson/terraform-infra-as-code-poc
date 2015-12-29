bc4929cdf44c3b3b2ecd82e0ab5d525bd71bf294
Ross Timson <ross@rosstimson.com>
2015-12-29 13:10:10 +0000

    Trying to fix AWS Creds via env vars.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  default = "{{env `AWS_ACCESS_KEY_ID`}}"
+}
+variable "aws_secret_key" {
+  default = "{{env `AWS_SECRET_ACCESS_KEY`}}"
+}
 variable "aws_region" {
   description = "AWS region to launch servers."
   default = "eu-west-1"
