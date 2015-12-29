629c23fb88e426219e311bd5c3bf5871f6f696dc
Ross Timson <ross@rosstimson.com>
2015-12-29 13:41:19 +0000

    Still trying to get creds / env vars working.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "aws_access_key" {
-  default = "{{env `AWS_ACCESS_KEY_ID`}}"
-}
-variable "aws_secret_key" {
-  default = "{{env `AWS_SECRET_ACCESS_KEY`}}"
-}
 variable "aws_region" {
   description = "AWS region to launch servers."
   default = "eu-west-1"
