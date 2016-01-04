ab8d5ab0dde91a68099cdaf4528f57af099f798f
Ross Timson <ross@rosstimson.com>
2016-01-04 16:58:47 +0000

    Show example of setting CORS rule.

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 provider "aws" {
-  region     = "${var.aws_region}"
+  region = "${var.aws_region}"
 }
 
 resource "aws_s3_bucket" "b" {
   bucket = "elsmc.tf.rosstimson"
   acl = "private"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT","POST"]
+    allowed_origins = ["elsmc.tf.rosstimson"]
+    expose_headers = ["ETag"]
+    max_age_seconds = 3000
+  }
   policy = <<POLICY
 {
   "Version": "2012-10-17",
